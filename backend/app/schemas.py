4a9e25f4ee06779a174b60dc55e4a6f921a5c43a
smileleft <spyt77@gmail.com>
2025-11-10 16:33:32 +0900

    Create schemas.py

---
+++ backend/app/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class Feature(BaseModel):
+type: str = "Feature"
+geometry: dict
+properties: dict
+
+
+class FeatureCollection(BaseModel):
+type: str = "FeatureCollection"
+features: list[Feature]
