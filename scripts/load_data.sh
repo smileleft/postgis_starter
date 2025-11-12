48081ff15748bb3ade935da7dad72ef88df30af3
smileleft <spyt77@gmail.com>
2025-11-12 11:14:48 +0900

    Update load_data.sh

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -16,16 +16,15 @@ echo "Loading $FILE into PostGIS…"
 
 
 # use ogr2ogr inside the gdal container; connect to the db service by name
-CMD="ogr2ogr -f PostgreSQL \"
-PG:host=${POSTGRES_HOST} port=${POSTGRES_PORT} dbname=${POSTGRES_DB} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}\" \
--nln seoul_districts \
--nlt MULTIPOLYGON \
--lco GEOMETRY_NAME=geom \
--lco FID=id \
--dialect SQLite \
--sql 'SELECT *, CAST(NULL AS INTEGER) AS id FROM \"seoul_municipalities.geojson\"' \
--a_srs EPSG:4326 \
--overwrite ${FILE}"
+CMD="ogr2ogr -f PostgreSQL \
+  PG:\"host=${POSTGRES_HOST} port=${POSTGRES_PORT} dbname=${POSTGRES_DB} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}\" \
+  /data/seoul_municipalities.geojson \
+  -nln seoul_districts \
+  -nlt MULTIPOLYGON \
+  -lco GEOMETRY_NAME=geom \
+  -lco FID=id \
+  -a_srs EPSG:4326 \
+  -overwrite"
 
 
 docker compose exec gdal sh -lc "$CMD"
